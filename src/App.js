d88e5220cf71d262e4b09c3d6be014be6bae19b1
Yauhen <silent_control@tut.by>
2020-07-16 20:44:08 +0300

    Rendering a Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const Search = ({ value, onChange, children }) => (
+  <div>
+    <label htmlFor="search">{children}</label>
+    <input id="search" type="text" value={value} onChange={onChange} />
+  </div>
+);
+
+const App = () => {
+  const [search, setSearch] = useState("");
+
+  const handleChange = ({ target }) => {
+    setSearch(target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Search value={search} onChange={handleChange}>
+        Search:
+      </Search>
+      <p>Searches for {search ? search : "..."}</p>
     </div>
   );
-}
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // const { getByText } = render(<App />);
+  // const linkElement = getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+
+  render(<App />);
+  screen.debug();
 });
