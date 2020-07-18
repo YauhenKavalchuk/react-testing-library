c714ca49bce7ff4f48f1c989a7742796529cb295
Yauhen <silent_control@tut.by>
2020-07-18 19:08:30 +0300

    Search variants & Assertive Functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+
+const getUser = () => Promise.resolve({ id: 1, name: "Yauhen" });
 
 const Search = ({ value, onChange, children }) => (
   <div>
@@ -11,12 +13,23 @@ const Search = ({ value, onChange, children }) => (
       type="text"
       value={value}
       onChange={onChange}
+      // required
     />
   </div>
 );
 
 const App = () => {
   const [search, setSearch] = useState("");
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const loadUser = async () => {
+      const user = await getUser();
+      setUser(user);
+    };
+
+    loadUser();
+  }, []);
 
   const handleChange = ({ target }) => {
     setSearch(target.value);
@@ -24,7 +37,8 @@ const App = () => {
 
   return (
     <div>
-      <img src="" alt="search image" />
+      {user && <h2>Logged in as {user.name}</h2>}
+      <img className="image" src="" alt="search image" />
       <Search value={search} onChange={handleChange}>
         SEARCH:
       </Search>
--- src/App.test.js
+++ src/App.test.js
@@ -2,14 +2,45 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+/*
+Search variants:
+
+  getBy:                    queryby:                    findBy:
+
+- getByText               - queryByText               - findByText
+- getByRole               - queryByRole               - findByRole
+- getByLabelText          - queryByLabelText          - findByLabelText
+- getByPlaceholderText    - queryByPlaceholderText    - findByPlaceholderText
+- getByAltText            - queryByAltText            - findByAltText
+- getByDisplayValue       - queryByDisplayValue       - findByDisplayValue
+- getAllBy                - queryAllBy                - findAllBy
+*/
+
+/*
+Assertive Functions:
+
+- toBeDisabled            - toBeEnabled               - toBeEmpty
+- toBeEmptyDOMElement     - toBeInTheDocument         - toBeInvalid
+- toBeRequired            - toBeValid                 - toBeVisible
+- toContainElement        - toContainHTML             - toHaveAttribute
+- toHaveClass             - toHaveFocus               - toHaveFormValues
+- toHaveStyle             - toHaveTextContent         - toHaveValue
+- toHaveDisplayValue      - toBeChecked               - toBePartiallyChecked
+- toHaveDescription
+*/
+
 describe("App", () => {
-  it("renders App component", () => {
+  it("renders App component", async () => {
     render(<App />);
-    expect(screen.getByText(/Search:/i)).toBeInTheDocument();
-    expect(screen.getByRole('textbox')).toBeInTheDocument();
-    expect(screen.getByLabelText(/search/i)).toBeInTheDocument();
-    expect(screen.getByPlaceholderText('search text...')).toBeInTheDocument();
-    expect(screen.getByAltText('search image')).toBeInTheDocument();
-    expect(screen.getByDisplayValue('')).toBeInTheDocument();
+    // expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+    expect(screen.queryByText(/Logged in as/)).toBeNull();
+    screen.debug();
+    expect(await screen.findByText(/Logged in as/)).toBeInTheDocument();
+    screen.debug();
+    // Assertive Functions Examples:
+    expect(screen.getByAltText(/search image/)).toHaveClass("image");
+    expect(screen.getByLabelText(/search/i)).not.toBeRequired();
+    expect(screen.getByLabelText(/search/i)).toBeEmpty();
+    expect(screen.getByLabelText(/search/i)).toHaveAttribute("id");
   });
 });
